48f819e777571c2a7341b8e5297ad8f216c86895
ZenG-8man <50492933+ZenG-8man@users.noreply.github.com>
2020-01-02 18:39:36 +0000

    Update get_emails.js

--- get_emails.js
+++ get_emails.js
@@ -2,4 +2,4 @@ var search_in = document.body.innerHTML;
 string_context = search_in.toString();
 
 array_mails = string_context.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-print(array_mails);
+console.log(array_mails);
