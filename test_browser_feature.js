47b8963bd247b44378ea6f4388fe1683cfc6565a
ZenG-8man <50492933+ZenG-8man@users.noreply.github.com>
2020-03-09 15:32:28 +0530

    Create test_browser_feature.js

---
+++ test_browser_feature.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.indexOf("Chrome") !== -1){
+    console.log("Chrome")
+} else if (navigator.userAgent.indexOf("Mozilla") !== -1){
+    console.log("Mozilla")
+} else { console.log("Other") }
